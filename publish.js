71d831437fef96f37eebceb9b49ee51cb0132697
David Wood <david.p.wood@gmail.com>
2016-08-21 13:20:31 +0100

    Copy js and map files to vendor

--- publish.js
+++ publish.js
@@ -2,22 +2,26 @@
 
 const cpr = require('cpr');
 
-function cp(from, to) {
+function cp(from, to, options) {
+  var o = options || {};
   return new Promise((resolve, reject) => {
-    cpr(from, to, (err, files) => {
+    cpr(from, to, o, (err, files) => {
       if (err) return reject(err);
       resolve();
     });
   });
 }
 
+const matchJS = /^.*\.js(\.map)?$/i;
+const js = (filename) => matchJS.test(filename);
+
 Promise.all([
-  cp('node_modules/core-js/client/shim.min.js', 'docs/vendor/core-js/client/shim.min.js'),
+  cp('node_modules/core-js/client/', 'docs/vendor/core-js/client/', { filter: js }),
   cp('node_modules/zone.js/dist/zone.min.js', 'docs/vendor/zone.js/dist/zone.min.js'),
-  cp('node_modules/reflect-metadata/Reflect.js', 'docs/vendor/reflect-metadata/Reflect.js'),
+  cp('node_modules/reflect-metadata/', 'docs/vendor/reflect-metadata/', { filter: js }),
   cp('node_modules/systemjs/dist/system.js', 'docs/vendor/systemjs/dist/system.js'),
-  cp('node_modules/@angular/', 'docs/vendor/@angular/'),
-  cp('node_modules/rxjs/', 'docs/vendor/rxjs/'),
+  cp('node_modules/@angular/', 'docs/vendor/@angular/', { filter: js }),
+  cp('node_modules/rxjs/', 'docs/vendor/rxjs/', { filter: js }),
   cp('systemjs.config.js', 'docs/systemjs.config.js'),
   cp('index.html', 'docs/index.html'),
 ]).catch((err) => {
